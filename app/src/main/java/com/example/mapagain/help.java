d28ebb7b21732c3ee413bcbf4f0c756d573c48f3
sarra halou <halousarra@gmail.com>
2023-06-15 10:54:46 +0100

    init2

--- app/src/main/java/com/example/mapagain/help.java
+++ app/src/main/java/com/example/mapagain/help.java
@@ -14,10 +14,9 @@ import android.widget.ViewSwitcher;
 
 public class help extends AppCompatActivity {
 
-    private ImageSwitcher imageSwitcher;
-    private TextView prec,suiv;
+    private ImageSwitcher imageSwitcher1,imageSwitcher2,imageSwitcher3;
 
-    private int[] images = {
+    private int[] images1 = {
             R.drawable.p1,
             R.drawable.p2,
             R.drawable.p3,
@@ -26,6 +25,19 @@ public class help extends AppCompatActivity {
             R.drawable.p6,
             R.drawable.p7,
             R.drawable.p8,
+    };
+
+    private int[] images2 = {
+            R.drawable.p9,
+            R.drawable.p10,
+            R.drawable.p11,
+            R.drawable.p12,
+    };
+
+    private int[] images3 = {
+            R.drawable.p15,
+            R.drawable.p16,
+            R.drawable.p17,
     };
 
     private int currentIndex = 0;
@@ -39,9 +51,11 @@ public class help extends AppCompatActivity {
 
 
 
-        imageSwitcher = findViewById(R.id.imageSwitcher1);
+        imageSwitcher1 = findViewById(R.id.imageSwitcher1);
+        imageSwitcher2 = findViewById(R.id.imageSwitcher2);
 
-        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+
+        imageSwitcher1.setFactory(new ViewSwitcher.ViewFactory() {
             @Override
             public View makeView() {
                 ImageView imageView = new ImageView(getApplicationContext());
@@ -56,20 +70,113 @@ public class help extends AppCompatActivity {
 
         Animation slideInAnimation = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
         Animation slideOutAnimation = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
-        imageSwitcher.setInAnimation(slideInAnimation);
-        imageSwitcher.setOutAnimation(slideOutAnimation);
+        imageSwitcher1.setInAnimation(slideInAnimation);
+        imageSwitcher1.setOutAnimation(slideOutAnimation);
 
-        imageSwitcher.setImageResource(images[currentIndex]);
+        imageSwitcher1.setImageResource(images1[currentIndex]);
 
-        imageSwitcher.setOnClickListener(new View.OnClickListener() {
+        imageSwitcher1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 currentIndex++;
-                if (currentIndex == images.length) {
+                if (currentIndex == images1.length) {
                     currentIndex = 0;
                 }
-                imageSwitcher.setImageResource(images[currentIndex]);
+                imageSwitcher1.setImageResource(images1[currentIndex]);
             }
         });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        imageSwitcher2.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(
+                        ImageSwitcher.LayoutParams.MATCH_PARENT,
+                        ImageSwitcher.LayoutParams.MATCH_PARENT
+                ));
+                return imageView;
+            }
+        });
+
+        Animation slideInAnimation2 = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        Animation slideOutAnimation2 = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+        imageSwitcher2.setInAnimation(slideInAnimation2);
+        imageSwitcher2.setOutAnimation(slideOutAnimation2);
+
+        imageSwitcher2.setImageResource(images2[currentIndex]);
+
+        imageSwitcher2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex++;
+                if (currentIndex == images2.length) {
+                    currentIndex = 0;
+                }
+                imageSwitcher2.setImageResource(images2[currentIndex]);
+            }
+        });
+
+
+
+
+
+
+        imageSwitcher3.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(
+                        ImageSwitcher.LayoutParams.MATCH_PARENT,
+                        ImageSwitcher.LayoutParams.MATCH_PARENT
+                ));
+                return imageView;
+            }
+        });
+
+        Animation slideInAnimation3 = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        Animation slideOutAnimation3 = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+        imageSwitcher3.setInAnimation(slideInAnimation3);
+        imageSwitcher3.setOutAnimation(slideOutAnimation3);
+
+        imageSwitcher3.setImageResource(images3[currentIndex]);
+
+        imageSwitcher3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex++;
+                if (currentIndex == images3.length) {
+                    currentIndex = 0;
+                }
+                imageSwitcher3.setImageResource(images3[currentIndex]);
+            }
+        });
+
     }
 }
