deda6b6b6b8c1156b357da061ac55e325bfb8650
sarra halou <halousarra@gmail.com>
2023-06-15 08:10:02 +0100

    init1

--- app/src/main/java/com/example/mapagain/help.java
+++ app/src/main/java/com/example/mapagain/help.java
@@ -2,13 +2,74 @@ package com.example.mapagain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 public class help extends AppCompatActivity {
+
+    private ImageSwitcher imageSwitcher;
+    private TextView prec,suiv;
+
+    private int[] images = {
+            R.drawable.p1,
+            R.drawable.p2,
+            R.drawable.p3,
+            R.drawable.p4,
+            R.drawable.p5,
+            R.drawable.p6,
+            R.drawable.p7,
+            R.drawable.p8,
+    };
+
+    private int currentIndex = 0;
+
+    @SuppressLint("MissingInflatedId")
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+
+
+        imageSwitcher = findViewById(R.id.imageSwitcher1);
+
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(
+                        ImageSwitcher.LayoutParams.MATCH_PARENT,
+                        ImageSwitcher.LayoutParams.MATCH_PARENT
+                ));
+                return imageView;
+            }
+        });
+
+        Animation slideInAnimation = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        Animation slideOutAnimation = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+        imageSwitcher.setInAnimation(slideInAnimation);
+        imageSwitcher.setOutAnimation(slideOutAnimation);
+
+        imageSwitcher.setImageResource(images[currentIndex]);
+
+        imageSwitcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex++;
+                if (currentIndex == images.length) {
+                    currentIndex = 0;
+                }
+                imageSwitcher.setImageResource(images[currentIndex]);
+            }
+        });
     }
 }
